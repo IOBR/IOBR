879ba8fafb7b8b6fc8968f5839e8306bddabcc5f
DongqiangZeng0808 <dongqiangzeng0808@gmail.com>
2020-11-06 14:49:37 +0800

    update find_mutations

--- R/find_mutations.R
+++ R/find_mutations.R
@@ -5,21 +5,25 @@
 
 
 
-#' Title
+#' Searching for mutations related to signature score
 #'
-#' @param mutation_matrix
-#' @param signature_matrix
-#' @param id_signature_matrix
-#' @param signature
-#' @param mutation_freq
-#' @param plot
-#' @param method
-#'
+#' @param mutation_matrix mutation matrix with sample name in the row and genes in the column
+#' @param signature_matrix signature data frame with identifier and target signature
+#' @param id_signature_matrix column name of identifier
+#' @param signature name of target signature
+#' @param min_mut_freq minimum frequency of mutation
+#' @param plot logical variable, if TRUE, plot will be save in the `save_path`
+#' @param method `multi` or Wilcoxon test only, if `multi` is applied, both `cuzick test` and `Wilcoxon` will be performed
+#' @param save_path path to save plot and statistical analyses
+#' @param palette palette of box plot
+#' @param show_plot logical variable, if TRUE, plot will be printed.
+#' @param show_col show code of palette
+#' @author Dongqiang Zeng
 #' @return
 #' @export
 #'
 #' @examples
-find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix = "ID", signature,mutation_freq = 0.05,plot = TRUE, method = "multi",save_path = NULL,palette = "paired3"){
+find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix = "ID", signature,min_mut_freq = 0.05,plot = TRUE, method = "multi",save_path = NULL,palette = "paired3",show_plot = TRUE,show_col = FALSE){
 
 
 
@@ -39,12 +43,11 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
     mutation_matrix[mutation_matrix>5]<-4
   }
 
-  #filter genes
   mut2<-mutation_matrix
   mut2[mut2>=1]<-1
   mutfreq<-data.frame(head(sort(colSums(mut2),decreasing = T),500))
   colnames(mutfreq)<-"Freq"
-  index<-which(mutfreq$Freq>=dim(mut2)[1]*mutation_freq)
+  index<-which(mutfreq$Freq>=dim(mut2)[1]*min_mut_freq)
   index<-max(index)
   input_genes<-names(head(sort(colSums(mut2),decreasing = T),index))
   input_genes<-unique(input_genes)
@@ -63,25 +66,22 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
   sig_mut<-tibble:: column_to_rownames(sig_mut,var = "ID")
 
 
-  mytheme<-theme_light()+   ###theme_bw()  就是EXCEL的格式但是还带有表格
+  mytheme<-theme_light()+
     theme(
       plot.title=element_text(size=rel(2.8),hjust=0.5),
       axis.title.y=element_text(size=rel(2.5)),
       axis.title.x= element_blank(),
       axis.text.x= element_text(face="plain",size=30,angle=0,color="black"),#family="Times New Roman"
       axis.text.y = element_text(face="plain",size=20,angle=90,color="black"),#family="Times New Roman"
-      # panel.grid.major=element_line(color="white"), ####变成白色就是看不到了
-      # panel.grid.minor=element_line(color="white"),  ####变成白色就是看不到了
-      # panel.border=element_rect(color="white"),   ####变成白色就是看不到了
       axis.line=element_line(color="black",size=0.70))+theme(
-        legend.key.size=unit(.3,"inches"),#图例分类符号的大小
+        legend.key.size=unit(.3,"inches"),
         legend.title=element_blank(),
-        legend.position="none",#"none","left","right","bottom","top",or #c(0.5,1)
-        legend.direction="horizontal",# 图例排列方向#,"vertical"
-        legend.justification=c(.5,.5),#"center" or two-element numeric vector
-        legend.box="vertical",#"horizontal",对图例的排列方式
-        legend.box.just="top",#多图例的居中方式
-        legend.text=element_text(colour="black",size=10,face = "plain")#("plain", "italic", "bold", "bold.italic")
+        legend.position="none",
+        legend.direction="horizontal",
+        legend.justification=c(.5,.5),
+        legend.box="vertical",
+        legend.box.just="top",
+        legend.text=element_text(colour="black",size=10,face = "plain")
       )
 
 
@@ -96,17 +96,15 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
                     names=input_genes,
                     statistic = sapply(aa, getElement, name = "statistic"))
     res1$adjust_pvalue<-p.adjust(res1$p.value,method = "BH",n=length(res1$p.value))
-    message(">>>> cuzickTest")
-    print(res1[res1$p.value<0.05,])
+    print(">>>> Result of Cuzick Test")
     res1<-res1[order(res1$p.value,decreasing = F),]
+    print(res1[1:10,])
 
     write.csv(res1,paste0(abspath,"1-cuzickTest-test-relevant-mutations.csv"))
 
 
     if(plot){
-
       ####################################
-      #' choose top 10 genes
       top10_genes<-res1$names[1:10]
       top10_genes<- top10_genes[!is.na(top10_genes)]
       top10_genes<-as.character(top10_genes[top10_genes%in%colnames(input)])
@@ -125,7 +123,7 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
         pl[[i]]<-ggplot(dd, aes(x=mutation, y = !!sym(signature), fill=mutation)) +
           geom_boxplot(outlier.shape = NA,outlier.size = NA)+
           geom_jitter(width = 0.25,size= 5.9,alpha=0.75,color ="black")+
-          scale_fill_manual(values=mydb:: my_palette(category = "box",palette = palette))+
+          scale_fill_manual(values = palettes(category = "box",palette = palette,show_col = show_col))+
           mytheme+theme(legend.position="none")+
           ggtitle(paste0(top10_genes[i]))+
           mytheme+stat_compare_means(comparisons = combn(as.character(unique(dd[,"mutation"])), 2, simplify=F),size=6)+
@@ -133,15 +131,14 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
         ggsave(pl[[i]],filename = paste0(4+i,"-1-",gene,"-continue.pdf"),
                width = 4.2,height = 6.5,path = file_name)
       }
-      # paste("pl","[[",1:10,"]]",sep = "",collapse=",")
       com_plot<- cowplot::plot_grid(pl[[1]],pl[[2]],pl[[3]],pl[[4]],pl[[5]],pl[[6]],pl[[7]],pl[[8]],pl[[9]],pl[[10]],
                       labels = "AUTO",ncol = 5,nrow = 2,label_size = 36)
+      if(show_plot) print(com_plot)
       #####################################
       ggsave(com_plot,filename = "3-Relevant_mutations_Continue.pdf",width = 25,height = 17,path = file_name)
       #####################################
     }
 
-    ################################
     sig_mut2<- rownames_to_column(sig_mut,var = "ID")
 
 
@@ -152,8 +149,7 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
     sig_mut2<-column_to_rownames(sig_mut2,var = "ID")
 
     input2<-sig_mut2
-    #' willcoxon test
-    ##############################
+
     aa<-lapply(input2[,input_genes], function(x) wilcox.test(input2[,1]~x))
 
     res2<-data.frame(p.value = sapply(aa, getElement, name = "p.value"),
@@ -161,13 +157,10 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
                     statistic = sapply(aa, getElement, name = "statistic"))
 
     res2$adjust_pvalue<-p.adjust(res2$p.value,method = "BH",n=length(res2$p.value))
-    res2[res2$p.value<0.05,]
+
     res2<-res2[order(res2$p.value,decreasing = F),]
-
-
-    message(">>>> wilcoxon test")
-    print(res2[res2$p.value<0.05,])
-
+    print(">>>> Result of Wilcoxon test")
+    print(res2[1:10,])
     write.csv(res2, paste0(abspath,"2-Wilcoxon-test-relevant-mutations.csv"))
 
     result<-list("cuzick_test" = res1, "wilcoxon_test" = res2,
@@ -177,7 +170,6 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
     if(plot){
 
       ####################################
-      #' choose top 10 genes
       top10_genes<-res2$names[1:10]
       top10_genes<- top10_genes[!is.na(top10_genes)]
       top10_genes<-as.character(top10_genes[top10_genes%in%colnames(input2)])
@@ -200,7 +192,7 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
         pl[[i]]<-ggplot(dd, aes(x=mutation, y = !!sym(signature), fill=mutation)) +
           geom_boxplot(outlier.shape = NA,outlier.size = NA)+
           geom_jitter(width = 0.25,size=5.5,alpha=0.75,color ="black")+
-          scale_fill_manual(values= mydb:: my_palette(category = "box",palette = palette))+
+          scale_fill_manual(values= palettes(category = "box",palette = palette,show_col = show_col))+
           mytheme+
           theme(legend.position="none")+
           ggtitle(paste0(top10_genes[i]))+
@@ -209,9 +201,9 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
         ggsave(pl[[i]],filename = paste0(4+i,"-2-",gene,"-binary.pdf"),
                width = 4.2,height = 6.5,path = file_name)
       }
-      # paste("pl","[[",1:10,"]]",sep = "",collapse=",")
       com_plot<-cowplot:: plot_grid(pl[[1]],pl[[2]],pl[[3]],pl[[4]],pl[[5]],pl[[6]],pl[[7]],pl[[8]],pl[[9]],pl[[10]],
                           labels = "AUTO",ncol = 5,nrow = 2,label_size = 36)
+      if(show_plot) print(com_plot)
       #####################################
       ggsave(com_plot,filename = "4-Relevant_mutations_binary.pdf",width = 22,height = 17,path = file_name)
       #####################################
@@ -226,7 +218,7 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
     sig_ms2<-cbind(patr1,part2)
 
     input2<-sig_ms2
-    #' willcoxon test
+
     ##############################
     aa<-lapply(input2[,input_genes], function(x) wilcox.test(input2[,1]~x))
 
@@ -235,8 +227,11 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
                      statistic = sapply(aa, getElement, name = "statistic"))
 
     res$adjust_pvalue<-p.adjust(res$p.value,method = "BH",n=length(res$p.value))
-    res[res$p.value<0.05,]
-    res<-res[order(res$p.value,decreasing = F),]
+
+    res2<-res2[order(res2$p.value,decreasing = F),]
+    print(">>>> Result of Wilcoxon test")
+    print(res2[1:10,])
+
 
     write.csv(res, paste0(abspath,"1-Wilcoxon-test-relevant-mutations.csv"))
 
@@ -247,7 +242,6 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
     if(plot){
 
       ####################################
-      #' choose top 10 genes
       top10_genes<-res$names[1:10]
       top10_genes<-as.character(top10_genes[top10_genes%in%colnames(input2)])
 
@@ -267,7 +261,7 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
         pl[[i]]<-ggplot(dd, aes(x=mutation, y = !!sym(signature), fill=mutation)) +
           geom_boxplot(outlier.shape = NA,outlier.size = NA)+
           geom_jitter(width = 0.25,size=5.5,alpha=0.75,color ="black")+
-          scale_fill_manual(values= mydb::my_palette(category = "box",palette = palette))+
+          scale_fill_manual(values= palettes(category = "box",palette = palette,show_col = show_col))+
           mytheme+theme(legend.position="none")+
           ggtitle(paste0(top10_genes[i]))+
           mytheme+
@@ -276,9 +270,9 @@ find_mutations<-function(mutation_matrix, signature_matrix, id_signature_matrix 
         ggsave(pl[[i]],filename = paste0(2+i,"-1-",gene,"-binary.pdf"),
                width = 4.2,height = 6.5,path = file_name)
       }
-      # paste("pl","[[",1:10,"]]",sep = "",collapse=",")
       com_plot<-cowplot:: plot_grid(pl[[1]],pl[[2]],pl[[3]],pl[[4]],pl[[5]],pl[[6]],pl[[7]],pl[[8]],pl[[9]],pl[[10]],
                           labels = "AUTO",ncol = 5,nrow = 2,label_size = 36)
+      if(show_plot) print(com_plot)
       #####################################
       ggsave(com_plot,filename = "2-Relevant_mutations_binary.pdf",width = 22,height = 17,path = file_name)
       #####################################
--- R/make_mut_matrix.R
+++ R/make_mut_matrix.R
@@ -4,68 +4,65 @@
 
 
 
-#' Title
+#' Make mutation matrix using maf data
 #'
-#' @param maf 
-#' @param maf_data 
-#' @param isTCGA 
-#' @param category 
-#' @param Tumor_Sample_Barcode 
-#' @param Hugo_Symbol 
-#' @param Variant_Classification 
-#' @param Variant_Type 
+#' @param maf file name of maf data
+#' @param maf_data if maf data had beed loaded, this argument will be applied
+#' @param isTCGA logical variable,
+#' @param category if category is `multi`, four mutation matrix will be generated: all, snp, indel and frameshift
+#' @param Tumor_Sample_Barcode column name of tumor sample
+#' @param Hugo_Symbol column name of gene symbol
+#' @param Variant_Classification column name of variant classification: Frame_Shift_Del
+#' @param Variant_Type column name of variant type: SNP, INS, DEL
 #'
 #' @return
 #' @export
+#' @author Dongqian Zeng
 #'
 #' @examples
 make_mut_matrix<-function(maf = NULL, maf_data = NULL, isTCGA = TRUE, category = "multi",Tumor_Sample_Barcode = "Tumor_Sample_Barcode", Hugo_Symbol = "Hugo_Symbol", Variant_Classification = "Variant_Classification", Variant_Type = "Variant_Type"){
-  
-  
+
+
   if(!is.null(maf)){
-    
+
     mut_maf<-maftools:: read.maf(maf = maf,useAll = FALSE,isTCGA = isTCGA)
-    
-    # summary(as.factor(substring(mut_maf@variants.per.sample$Tumor_Sample_Barcode,14,16)))
-    # summary(duplicated(substring(mut_maf@variants.per.sample$Tumor_Sample_Barcode,1,12)))
-    #########################################
-    # dim(mut_maf@data)
+
     print(summary(mut_maf@data$Variant_Classification))
     print(summary(mut_maf@data$Variant_Type))
     #########################################
-    
+
     mut<-mut_maf@data
   }else{
-    
+
     mut<-maf_data
     colnames(mut)[which(colnames(mut)==Tumor_Sample_Barcode)]<-"Tumor_Sample_Barcode"
     colnames(mut)[which(colnames(mut)==Hugo_Symbol)]<-"Hugo_Symbol"
     colnames(mut)[which(colnames(mut)==Variant_Classification)]<-"Variant_Classification"
     colnames(mut)[which(colnames(mut)==Variant_Type)]<-"Variant_Type"
-    
+
   }
-  
+
   mut<-mut[,c("Tumor_Sample_Barcode","Hugo_Symbol","Variant_Classification","Variant_Type")]
-  
+
   if(category == "multi"){
-    
+
     mut_all <-reshape2:: dcast(mut, Hugo_Symbol~Tumor_Sample_Barcode, value.var="Variant_Classification")
     mut_all<-tibble:: column_to_rownames(mut_all,var = "Hugo_Symbol")
     mut_all<-as.matrix(mut_all)
     mut_all<-matrix(as.numeric(mut_all), dim(mut_all), dimnames = dimnames(mut_all))
-    
+
     mut_snp<-mut[mut$Variant_Type=="SNP",];dim(mut_snp)
     mut_snp <-reshape2:: dcast(mut_snp, Hugo_Symbol~Tumor_Sample_Barcode, value.var="Variant_Classification")
     mut_snp<-tibble:: column_to_rownames(mut_snp,var = "Hugo_Symbol")
     mut_snp<-as.matrix(mut_snp)
     mut_snp<-matrix(as.numeric(mut_snp), dim(mut_snp), dimnames = dimnames(mut_snp))
-    
+
     mut_indel<-mut[mut$Variant_Type=="DEL"|mut$Variant_Type=="INS",];dim(mut_indel)
     mut_indel <- reshape2::dcast(mut_indel, Hugo_Symbol~Tumor_Sample_Barcode, value.var="Variant_Classification")
     mut_indel<-tibble:: column_to_rownames(mut_indel,var = "Hugo_Symbol")
     mut_indel<-as.matrix(mut_indel)
     mut_indel<-matrix(as.numeric(mut_indel), dim(mut_indel), dimnames = dimnames(mut_indel))
-    
+
     mut_frameshift<-mut[mut$Variant_Classification=="Frame_Shift_Del"|mut$Variant_Classification=="Frame_Shift_Ins",]
     mut_frameshift <- reshape2::dcast(mut_frameshift, Hugo_Symbol~Tumor_Sample_Barcode, value.var="Variant_Classification")
     mut_frameshift<-tibble:: column_to_rownames(mut_frameshift,var = "Hugo_Symbol")
@@ -76,7 +73,7 @@ make_mut_matrix<-function(maf = NULL, maf_data = NULL, isTCGA = TRUE, category =
     mut_list = list(all = t(mut_all),snp=t(mut_snp),indel=t(mut_indel),frameshift = t(mut_frameshift))
     mut_list = ComplexHeatmap:: unify_mat_list(mut_list)
   }else{
-    
+
     mut<-mut[,c("Tumor_Sample_Barcode","Hugo_Symbol","Variant_Classification","Variant_Type")]
     mut_all <-reshape2:: dcast(mut, Hugo_Symbol~Tumor_Sample_Barcode, value.var="Variant_Classification")
     mut_all<-tibble:: column_to_rownames(mut_all,var = "Hugo_Symbol")
