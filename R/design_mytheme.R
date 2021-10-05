e17c3a4f97d40072af4661aec2c2c711b61af9f2
DongqiangZeng0808 <dongqiangzeng0808@gmail.com>
2021-10-05 23:20:58 +0800

    update mytheme

--- R/design_mytheme.R
+++ R/design_mytheme.R
@@ -13,6 +13,7 @@
 #' @param legend.direction legend.direction horizontal or vertical
 #' @param hjust horizontal justification
 #' @param legend.size font size of legend
+#' @param theme default is light
 #'
 #' @author Dongqiang Zeng
 #' @return mytheme
@@ -20,31 +21,58 @@
 #'
 #' @examples
 #' mytheme <- design_mytheme()
-design_mytheme<-function( plot_title_size = 2,axis_title_size = 2,
+design_mytheme<-function(theme = "light",
+                         plot_title_size = 2,axis_title_size = 2,
                           axis_text_size = 12, axis_angle = 60,
                           hjust = 1,legend.position = "bottom",
                           legend.direction="horizontal", legend.size = 12){
 
-  mytheme<-ggplot2:: theme_light()+   ###theme_bw()
-   ggplot2:: theme(plot.title=element_text(size=rel(plot_title_size),hjust=0.5),
-          axis.title=element_text(size=rel(axis_title_size)),
-          axis.text=element_text(size=rel(2.5)),
-          axis.text.x= element_text(face="plain",size= axis_text_size,
-                                    angle=axis_angle,hjust = hjust,color="black"),#family="Times New Roman"
-          axis.text.y= element_text(face="plain",size= axis_text_size,color="black"),#family="Times New Roman"
-          # panel.grid.major=element_line(color="white"),
-          # panel.grid.minor=element_line(color="white"),
-          # panel.border=element_rect(color="white"),
-          axis.line=element_line(color="black",size=0.5))+theme(
-            legend.key.size=unit(.3,"inches"),
-            legend.title=element_blank(),
-            legend.position= legend.position,#"none","left","right","bottom","top",or #c(0.5,1)
-            legend.direction= legend.direction,# "vertical"
-            legend.justification=c(.5,.5),#"center" or two-element numeric vector
-            legend.box="horizontal",#"horizontal",
-            legend.box.just="top",
-            legend.text=element_text(colour="black",size=legend.size,face = "plain")#("plain", "italic", "bold", "bold.italic")
-          )
+  if(theme=="light"){
+    mytheme<-ggplot2:: theme_light()+   ###theme_bw()
+      ggplot2:: theme(plot.title=element_text(size=rel(plot_title_size),hjust=0.5),
+                      axis.title=element_text(size=rel(axis_title_size)),
+                      axis.text=element_text(size=rel(2.5)),
+                      axis.text.x= element_text(face="plain",size= axis_text_size,
+                                                angle=axis_angle,hjust = hjust,color="black"),#family="Times New Roman"
+                      axis.text.y= element_text(face="plain",size= axis_text_size,color="black"),#family="Times New Roman"
+                      # panel.grid.major=element_line(color="white"),
+                      # panel.grid.minor=element_line(color="white"),
+                      # panel.border=element_rect(color="white"),
+                      axis.line=element_line(color="black",size=0.5))+theme(
+                        legend.key.size=unit(.3,"inches"),
+                        legend.title=element_blank(),
+                        legend.position= legend.position,#"none","left","right","bottom","top",or #c(0.5,1)
+                        legend.direction= legend.direction,# "vertical"
+                        legend.justification=c(.5,.5),#"center" or two-element numeric vector
+                        legend.box="horizontal",#"horizontal",
+                        legend.box.just="top",
+                        legend.text=element_text(colour="black",size=legend.size,face = "plain")#("plain", "italic", "bold", "bold.italic")
+                      )
+  }else{
+    mytheme<-ggplot2:: theme_bw()+   ###theme_bw()
+      ggplot2:: theme(plot.title=element_text(size=rel(plot_title_size),hjust=0.5),
+                      axis.title=element_text(size=rel(axis_title_size)),
+                      axis.text=element_text(size=rel(2.5)),
+                      axis.text.x= element_text(face="plain",size= axis_text_size,
+                                                angle=axis_angle,hjust = hjust,color="black"),#family="Times New Roman"
+                      axis.text.y= element_text(face="plain",size= axis_text_size,color="black"),#family="Times New Roman"
+                      # panel.grid.major=element_line(color="white"),
+                      # panel.grid.minor=element_line(color="white"),
+                      # panel.border=element_rect(color="white"),
+                      axis.line=element_line(color="black",size=0.5))+theme(
+                        legend.key.size=unit(.3,"inches"),
+                        legend.title=element_blank(),
+                        legend.position= legend.position,#"none","left","right","bottom","top",or #c(0.5,1)
+                        legend.direction= legend.direction,# "vertical"
+                        legend.justification=c(.5,.5),#"center" or two-element numeric vector
+                        legend.box="horizontal",#"horizontal",
+                        legend.box.just="top",
+                        legend.text=element_text(colour="black",size=legend.size,face = "plain")#("plain", "italic", "bold", "bold.italic")
+                      )
+  }
+
+
+
   return(mytheme)
 }
 
