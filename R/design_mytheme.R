dc61b8a3c7988069f8c5035ac3fca6a56264e257
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2022-10-30 00:12:59 +0800

    Update design_mytheme.R

--- R/design_mytheme.R
+++ R/design_mytheme.R
@@ -15,6 +15,8 @@
 #' @param legend.size font size of legend
 #' @param theme default is light
 #' @param legend.size.text default is 12
+#' @param legend.key.height default is 0.5
+#' @param legend.key.width default is 0.5
 #'
 #' @author Dongqiang Zeng
 #' @return mytheme
@@ -22,11 +24,18 @@
 #'
 #' @examples
 #' mytheme <- design_mytheme()
-design_mytheme<-function(theme = "light",
-                         plot_title_size = 2,axis_title_size = 2,
-                          axis_text_size = 12, axis_angle = 60,
-                          hjust = 1,legend.position = "bottom",
-                          legend.direction="horizontal", legend.size = 0.25,legend.size.text = 12){
+design_mytheme<-function(theme            = "light",
+                         plot_title_size  = 2,
+                         axis_title_size  = 2,
+                         axis_text_size   = 12,
+                         axis_angle       = 60,
+                         hjust            = 1,
+                         legend.position  = NULL,
+                         legend.direction = NULL,
+                         legend.size      = 0.25,
+                         legend.key.height= 0.5,
+                         legend.key.width = 0.5,
+                         legend.size.text = 10){
 
   if(theme=="light"){
     mytheme<-ggplot2::theme_light()
@@ -37,29 +46,45 @@ design_mytheme<-function(theme = "light",
   }else if(theme=="classic2"){
     mytheme<-ggpubr:: theme_classic2()
   }
-  message(paste0(">>>> theme: ",theme, ". Options: light, bw, classic and classic2"))
+  message(paste0(">>>>Options for `theme`: light, bw, classic and classic2"))
 
-  mytheme<-mytheme+ ggplot2:: theme(plot.title=element_text(size=rel(plot_title_size),hjust=0.5),
+  mytheme<-mytheme + ggplot2:: theme(plot.title=element_text(size=rel(plot_title_size),hjust=0.5),
                       axis.title=element_text(size=rel(axis_title_size)),
-                      axis.text=element_text(size=rel(2.5)),
+                      axis.text=element_text(size=rel(4)),
                       axis.text.x= element_text(face="plain",size= axis_text_size,
                                                 angle=axis_angle,hjust = hjust,color="black"),#family="Times New Roman"
                       axis.text.y= element_text(face="plain",size= axis_text_size,color="black"),#family="Times New Roman"
                       # panel.grid.major=element_line(color="white"),
                       # panel.grid.minor=element_line(color="white"),
                       # panel.border=element_rect(color="white"),
-                      axis.line=element_line(color="black",size=0.5))+
-                     theme(
-                        legend.key.size=unit(legend.size,"cm"),
-                        # legend.title=element_blank(),
-                        legend.position= legend.position,#"none","left","right","bottom","top",or #c(0.5,1)
-                        legend.direction= legend.direction,# "vertical"
-                        legend.justification=c(.5,.5),#"center" or two-element numeric vector
-                        legend.box="horizontal",#"horizontal",
-                        legend.box.just="top",
-                        legend.text=element_text(colour="black", size=legend.size.text, face = "plain")#("plain", "italic", "bold", "bold.italic")
-                      )
+                      axis.line=element_line(color="black",size=0.5))
 
+  if(is.null(legend.position)) {
+    legend.position<- "bottom"
+  }else{
+    message(paste0(">>>>Options for 'legend.position' : none, left, right, bottom, top"))
+  }
+
+  if(is.null(legend.direction)) {
+    legend.direction<- "horizontal"
+  }else{
+    message(paste0(">>>>Options for 'legend.direction' : horizontal, vertical "))
+  }
+
+
+  mytheme<-mytheme+
+    theme(
+      # legend.key.size=unit(legend.size,"cm"),
+      legend.key.height= unit(legend.key.height, 'cm'),
+      legend.key.width= unit(legend.key.width, 'cm'),
+      # legend.title=element_blank(),
+      legend.position= legend.position,#"none","left","right","bottom","top",or #c(0.5,1)
+      legend.direction= legend.direction,# "vertical"
+      legend.justification=c(.5,.5),#"center" or two-element numeric vector
+      legend.box="horizontal",#"horizontal",
+      legend.box.just="top",
+      legend.text=element_text(colour="black", size= legend.size.text, face = "plain")#("plain", "italic", "bold", "bold.italic")
+    )
 
   return(mytheme)
 }
