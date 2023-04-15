6f7d869d0eb354dc50d611237f49d03430e72f73
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2023-04-15 23:30:16 +0800

    add find_markers_in_bulk

--- R/check_eset.R
+++ R/check_eset.R
@@ -5,12 +5,13 @@
 #'
 #' @param eset gene expression set
 #' @param print_result logical, default is FALSE
+#' @param estimate_sd sd estimation
 #'
 #' @return
 #' @export
 #'
 #' @examples
-check_eset<-function(eset, print_result = F){
+check_eset<-function(eset, print_result = F , estimate_sd = FALSE){
 
 
 
@@ -20,7 +21,7 @@ check_eset<-function(eset, print_result = F){
 
   }
 
-  if(sum(is.na(eset))>0)  warning(paste0("There are some missing values in the gene expression matrix, which may affect the score calculation. You can set parameter 'adjust_eset' as TRUE to avoid these effects"))
+  if(sum(is.na(eset))>0)  warning(paste0("There are some missing values in the matrix, which may affect the score calculation. You can set parameter 'adjust_eset' as TRUE to avoid these effects"))
 
 
   teset<-as.data.frame(t(eset))
@@ -36,17 +37,18 @@ check_eset<-function(eset, print_result = F){
     print(summary(lapply(teset,function(x) max(x))==Inf))
   }
 
-  if(min(eset)==-Inf | max(eset)== Inf)  warning(paste0("There are some infinite values in the gene expression matrix, which may affect the score calculation. You can set parameter 'adjust_eset' as TRUE to avoid these effects"))
+  if(min(eset)==-Inf | max(eset)== Inf)  warning(paste0("There are some infinite values in the matrix, which may affect the score calculation. You can set parameter 'adjust_eset' as TRUE to avoid these effects"))
 
 
-  sd<-apply(eset,1,function(x) sd(x)==0)
+  if(estimate_sd){
+    sd<-apply(eset,1,function(x) sd(x)==0)
 
-  if(print_result){
-    message(paste0("<<< Features have sd = 0 >>> "))
-    print(summary(sd))
+    if(print_result){
+      message(paste0("<<< Features have sd = 0 >>> "))
+      print(summary(sd))
+    }
+
+    if(nlevels(as.factor(sd))>1) warning(paste0("Some vairables in the matrix have no variance between samples, which may affect the score calculation. You can set parameter 'adjust_eset' as TRUE to avoid these effects"))
+
   }
-
-  if(nlevels(as.factor(sd))>1) warning(paste0("Some vairables in the gene expression matrix have no variance between samples, which may affect the score calculation. You can set parameter 'adjust_eset' as TRUE to avoid these effects"))
-
-
 }
--- R/find_markers_in_bulk.R
+++ R/find_markers_in_bulk.R
@@ -3,25 +3,31 @@
 
 #' Title
 #'
-#' @param pdata 
-#' @param eset 
-#' @param group 
-#' @param id_pdata 
-#' @param nfeatures 
-#' @param top_n 
-#' @param thresh.use 
-#' @param only.pos 
-#' @param min.pct 
+#' @param pdata
+#' @param eset
+#' @param group
+#' @param id_pdata
+#' @param nfeatures
+#' @param top_n
+#' @param thresh.use
+#' @param only.pos
+#' @param min.pct
 #'
 #' @return
 #' @export
 #'
 #' @examples
 find_markers_in_bulk<-function(pdata, eset, group, id_pdata = "ID", nfeatures = 200, top_n = 20, thresh.use = 0.25, only.pos = TRUE, min.pct = 0.25){
+
+
   pdata<-column_to_rownames(pdata, var = id_pdata)
+  pdata<-pdata[rownames(pdata)%in%colnames(eset),]
+
   feas<-rownames(eset)
   feas<-feature_manipulation(data = eset, feature = feas, is_matrix = T)
+
   eset<-eset[rownames(eset)%in%feas, ]
+  eset<- eset[,colnames(eset)%in%rownames(pdata)]
   sce <- CreateSeuratObject(counts = eset,
                             meta.data = pdata,
                             min.cells = 0,
@@ -40,7 +46,7 @@ find_markers_in_bulk<-function(pdata, eset, group, id_pdata = "ID", nfeatures = 
   meta<-sce@meta.data
   Idents(sce)<-as.character( meta[,group])
   print(table(Idents(sce)))
-  sce.markers <- FindAllMarkers(object = sce, only.pos = only.pos, min.pct = min.pct, thresh.use = thresh.use)
+  sce.markers <- FindAllMarkers(object = sce, only.pos = only.pos, min.pct = min.pct, thresh.use = thresh.use, logfc.threshold = thresh.use)
   # sce.markers %>% group_by(cluster) %>% top_n(top_n, avg_log2FC)
   topN <- sce.markers %>% group_by(cluster) %>% top_n(top_n, avg_log2FC)
   print(topN)
