f859bcf9494bbdea8f0e7a6177f93668ebe1ca11
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2023-04-15 08:33:14 +0800

    Create find_outlier_samples.R

---
+++ R/find_outlier_samples.R
@@ -0,0 +1,56 @@
+
+
+
+
+#' Title
+#'
+#' @param eset expression set
+#' @param yinter between -3 to -2
+#' @param project default is NSCLC
+#'
+#' @return
+#' @export
+#'
+#' @examples
+find_outlier_samples<- function(eset, yinter = -3, project = "NSCLC"){
+  
+  
+  path<- creat_folder(project)
+  tree.combat <- eset %>% t %>% dist %>% hclust(method = "average")
+  CairoPDF(paste0(path$abspath, project_name,"-clusteringplot"), width = 20, height = 10)
+  plot(tree.combat, main =paste0("1-", project_name,"-","Hierarchical Clustering Sammples"))
+  dev.off()
+  ###############################
+  
+  #' @使用WGCNA查看样本离异度
+  ###############################
+  normalized.adjacency <- (0.5 + 0.5 * bicor(eset)) ^ 2
+  network.summary <- fundamentalNetworkConcepts(normalized.adjacency)
+  connectivity <- network.summary$Connectivity
+  connectivity.zscore <- (connectivity - mean(connectivity)) / sqrt(var(connectivity))
+  connectivity.plot <- data.frame(Sample.Name = names(connectivity.zscore),
+                                  Z.score = connectivity.zscore,
+                                  Sample.Num = 1:length(connectivity.zscore))
+  ################################
+  
+  p <- ggplot(connectivity.plot, aes(x = Sample.Num, y = Z.score, label = Sample.Name)) +
+    geom_text(size = 4, colour = "red")
+  p <- p + geom_hline(aes(yintercept = yinter))
+  p <- p + theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  p <- p + xlab("Sample Number") + ylab("Z score") + ggtitle("Sample Connectivity")
+  p<-p+design_mytheme()
+  
+  ggsave(p, filename = paste0("2-", project_name,"-connectivityplot.pdf"), width = 10, height = 10, path = path$folder_name)
+         
+  names_eset_rmout <- colnames(eset)[abs(connectivity.zscore) > abs(yinter)]
+  
+  message(paste0(">>>--- when yinter = ", yinter))
+  message(">>>--- Potential outliers: ")
+  print(names_eset_rmout)
+  
+  return(names_eset_rmout)
+}
+
+
+# find_outlier_samples(eset = eset, project = "melanoma")
+
