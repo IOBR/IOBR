f73c38da6a976e10b793e266c681db7d26f6a021
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2023-08-23 12:07:28 +0800

    Update sig_heatmap.R

--- R/sig_heatmap.R
+++ R/sig_heatmap.R
@@ -3,31 +3,33 @@
 
 
 
-#' simple heatmap
+#' @title sig_heatmap
+#' @description The sig_heatmap function is used to generate a heatmap plot based on input data, grouping variables, and optional conditions. The function allows customization of various parameters such as palette selection, scaling, color boxes, plot dimensions, and more. It provides flexibility in visualizing relationships between variables and groups in a concise and informative manner.
 #'
 #' @param input data frame with `ID`, `group` and features
-#' @param ID default is ID
-#' @param features features
-#' @param group category
-#' @param palette default is 2, options: 1,2,3
-#' @param show_col show color
-#' @param show_palettes show palettes
-#' @param show_plot show plot
-#' @param width default is 8
-#' @param show_heatmap_col_name  show heat map column name
-#' @param path save path, folder name
-#' @param index default is null
-#' @param palette_group default is `jama`
-#' @param height height of figure
-#' @param size_col font size of column name
-#' @param size_row font size of row name
-#' @param angle_col angle of column name
-#' @param cols_group user can define the colors of group
-#' @param column_title  title of column
-#' @param row_title title of row
-#' @param scale default is FALSE
-#' @param condiction
-#' @param id_condiction
+#' @param ID This parameter specifies the column name in the input data that represents the unique identifier. The default value is "ID".
+#' @param features This parameter is a vector of column names representing the features to be used in the heatmap.
+#' @param group This parameter is a column name in the input data that represents the grouping variable.
+#' @param palette This parameter specifies the palette to be used in the heatmap. It can be a numeric value representing the palette index or a string specifying the palette name. The default value is 2.
+#' @param show_col This parameter specifies whether to show the color boxes in the heatmap. If set to TRUE, the color boxes will be shown. The default value is FALSE.
+#' @param show_palettes This parameter specifies whether to show the available palettes. If set to TRUE, the available palettes will be shown. The default value is FALSE.
+#' @param show_plot This parameter specifies whether to show the resulting heatmap plot. If set to TRUE, the plot will be shown. The default value is TRUE.
+#' @param width This parameter specifies the width of the resulting heatmap plot in inches. The default value is 8.
+#' @param show_heatmap_col_name This parameter specifies whether to show the column names in the heatmap plot. If set to TRUE, the column names will be shown. The default value is FALSE.
+#' @param path This parameter specifies the directory path where the resulting heatmap plot will be saved. If not specified, a default path will be used.
+#' @param index default is nullThis parameter specifies the index number to be added to the filename of the resulting heatmap plot. If not specified, the index will be set to 1.
+#' @param palette_group This parameter specifies the palette group to be used in the heatmap. It should be a string representing the palette group name. The default value is "jama".
+#' @param height This parameter specifies the height of the resulting heatmap plot in inches. If not specified, the height will be calculated based on the number of features.
+#' @param size_col This parameter specifies the font size of the column names in the heatmap plot. The default value is 10.
+#' @param size_row This parameter specifies the font size of the row names in the heatmap plot. The default value is 8.
+#' @param angle_col This parameter specifies the rotation angle of the column names in the heatmap plot. The default value is 90.
+#' @param cols_group This parameter is a vector of colors to be used for the grouping variable. If not specified, the colors will be randomly assigned.
+#' @param column_title  This parameter specifies the title of the column in the heatmap plot. If not specified, no title will be displayed.
+#' @param row_title This parameter specifies the title of the row in the heatmap plot. If not specified, no title will be displayed.
+#' @param scale This parameter specifies whether to scale the heatmap by column. If set to TRUE, the heatmap will be scaled by column. The default value is FALSE.
+#' @param condiction This parameter is an optional data frame that defines additional conditions to be applied in the heatmap. It should have two columns: one specifying the variables and another specifying the conditions. The default value is NULL.
+#' @param id_condiction This parameter specifies the column name in the condiction data frame that represents the variables. The default value is "vars".
+#' @param col_condiction This parameter specifies the column name in the condiction data frame that represents the conditions. The default value is "condiction".
 #'
 #' @return
 #' @export
@@ -39,6 +41,7 @@ sig_heatmap<-function(input,
                       group,
                       condiction            = NULL,
                       id_condiction         = "vars",
+                      col_condiction        = "condiction",
                       scale                 = FALSE,
                       palette               = 2,
                       palette_group         = "jama",
@@ -83,10 +86,14 @@ sig_heatmap<-function(input,
 
 
   if(!is.null(condiction)){
+
+    condiction <- condiction[,c(id_condiction, col_condiction)]
+
     colnames(condiction)[which(colnames(condiction)==id_condiction)]<-"vars"
-    condiction <- condiction[,c("vars", "condiction")]
+    colnames(condiction)[which(colnames(condiction)==col_condiction)]<-"condiction"
+
     pf_long_group <- merge(pf_long_group, condiction, by.x = "variables", by.y = "vars", all.x = TRUE, all.y = FALSE)
-    pf_long_group$condiction  <- ifelse(is.na(pf_long_group$condiction), "others", pf_long_group$condiction)
+    pf_long_group$condiction  <- ifelse(is.na(pf_long_group$condiction), "NE", pf_long_group$condiction)
     print(head(pf_long_group))
   }
   ###################################################
