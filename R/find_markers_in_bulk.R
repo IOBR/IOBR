d7eb071e91ff31ad5aa225d1b2051fff22449fb2
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2023-08-24 09:26:20 +0800

    Update find_markers_in_bulk.R

--- R/find_markers_in_bulk.R
+++ R/find_markers_in_bulk.R
@@ -1,23 +1,24 @@
 
 
 
-#' Title
+#' Find markers in bulk
+#' @description The goal of this function is to find relevant results from the given gene expression data and meta information.
 #'
-#' @param pdata
-#' @param eset
-#' @param group
-#' @param id_pdata
-#' @param nfeatures
-#' @param top_n
-#' @param thresh.use
-#' @param only.pos
-#' @param min.pct
+#' @param pdata A data frame containing the meta information of the samples.
+#' @param eset A matrix containing the gene expression data or signature score.
+#' @param group A string representing the column name for grouping.
+#' @param id_pdata A string representing the column name for sample IDs, default is "ID".
+#' @param nfeatures A numeric value indicating the top n features to select from the variable features, default is 2000.
+#' @param top_n A numeric value representing the top n markers to select in each cluster, default is 20.
+#' @param thresh.use A numeric value representing the marker selection threshold, default is 0.25.
+#' @param only.pos A logical value indicating whether to select only positive markers, default is TRUE.
+#' @param min.pct A numeric value representing the minimum percentage threshold for marker selection, default is 0.25.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-find_markers_in_bulk<-function(pdata, eset, group, id_pdata = "ID", nfeatures = 200, top_n = 20, thresh.use = 0.25, only.pos = TRUE, min.pct = 0.25){
+find_markers_in_bulk<-function(pdata, eset, group, id_pdata = "ID", nfeatures = 2000, top_n = 20, thresh.use = 0.25, only.pos = TRUE, min.pct = 0.25){
 
 
   pdata<-column_to_rownames(pdata, var = id_pdata)
@@ -28,7 +29,7 @@ find_markers_in_bulk<-function(pdata, eset, group, id_pdata = "ID", nfeatures = 
 
   eset<-eset[rownames(eset)%in%feas, ]
   eset<- eset[,colnames(eset)%in%rownames(pdata)]
-  sce <- CreateSeuratObject(counts = eset,
+  sce <- Seurat:: CreateSeuratObject(counts = eset,
                             meta.data = pdata,
                             min.cells = 0,
                             min.features = 0,
@@ -38,9 +39,9 @@ find_markers_in_bulk<-function(pdata, eset, group, id_pdata = "ID", nfeatures = 
                    # vars.to.regress = c('nCount_RNA'),
                    model.use = 'linear',
                    use.umi = FALSE)
-  sce <- FindVariableFeatures(object = sce, nfeatures = nfeatures)
+  sce <- Seurat::FindVariableFeatures(object = sce, nfeatures = nfeatures)
   # length(VariableFeatures(sce))
-  sce <- RunPCA(object = sce, pc.genes = VariableFeatures(sce))
+  sce <- Seurat::RunPCA(object = sce, pc.genes = VariableFeatures(sce))
   # sce <- FindNeighbors(object = sce, dims = 1:20, verbose = FALSE)
   # sce <- FindClusters(object = sce, resolution = 0.5,verbose = FALSE)
   meta<-sce@meta.data
