5cbe01597cf4a0fd5c3198d2b02d74c8d1bc80b2
Dongqiang Zeng <dongqiangzeng0808@gmail.com>
2023-08-28 22:00:29 +0800

    Update get_cols.R

--- R/get_cols.R
+++ R/get_cols.R
@@ -5,7 +5,6 @@
 #' Set and view the color palettes
 #'
 #' @param cols Vector of colors, users can define the cols manually.  This may also be a single character, such as normal and random,  to a palette as specified by `palettes(){IOBR}`
-#'             See [palettes](http://127.0.0.1:60491/help/library/IOBR/html/palettes.html) for details
 #' @param palette Numeric value corresponding with color palette. Default is 1, other options: 2, 3, 4
 #' @param show_col Whether to show color palettes
 #' @param seed Seed of the random number generator, default is 123. The parameter works when cols ="random"
@@ -19,20 +18,25 @@ get_cols<-function(cols = "normal", palette = 1, show_col = T, seed = 123){
 
   ##############################################
   if(length(cols)==1){
-    if(cols=="random"){
 
-      mycols<-palettes(category = "random", palette = palette, show_col = show_col)
-      message(">>>> Default seed is 123, you can change it by `seed`(parameter).")
-      set.seed(seed)
-      mycols<-mycols[sample(length(mycols), length(mycols))]
-      if(show_col) scales::show_col(mycols)
+    if(nchar(palette) > 1){
+      cols <- palettes(category = "box", palette = palette, show_col = show_col, show_message = FALSE)
+      mycols<-cols
+    } else{
+      if(cols=="random"){
 
-    }else if(cols == "normal"){
+        mycols<-palettes(category = "random", palette = palette, show_col = show_col)
+        message(">>>> Default seed is 123, you can change it by `seed`(parameter).")
+        set.seed(seed)
+        mycols<-mycols[sample(length(mycols), length(mycols))]
+        if(show_col) scales::show_col(mycols)
 
-      mycols<-palettes(category = "random", palette = palette, show_col = show_col)
-    }else{
-      mycols<- palettes(category = "box", palette = cols, show_col = show_col)
+      }else if(cols == "normal"){
+
+        mycols<-palettes(category = "random", palette = palette, show_col = show_col)
+      }
     }
+
   }else{
     mycols<-cols
     if(show_col) scales::show_col(mycols)
